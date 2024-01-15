dd8aa175cd804ceab8bb80e06687550c98666523
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-01-15 17:49:34 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import { TaskCounter } from 'components/TaskCounter/TaskCounter';
+import css from './App.module.css';
+import { StatusFilter } from './StatusFilter/StatusFilter';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <header className={css.wrapper}>
+      <section className={css.section}>
+        <h2 className={css.title}>Tasks</h2>
+        <TaskCounter />
+      </section>
+      <section className={css.section}>
+        <h2 className={css.title}>Filter by status</h2>
+        <StatusFilter />
+      </section>
+    </header>
   );
 };
