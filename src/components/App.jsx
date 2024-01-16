21a4c9e10b5ca2d40689fa827d46834b1193521e
TokarevArtemV <tokarev.artem.v@gmail.com>
2024-01-16 23:41:07 +0200

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,14 @@
-import { TaskCounter } from 'components/TaskCounter/TaskCounter';
-import css from './App.module.css';
-import { StatusFilter } from './StatusFilter/StatusFilter';
+import { Layout } from 'components/Layout/Layout';
+import { AppBar } from 'components/AppBar/AppBar';
+import { TaskForm } from 'components/TaskForm/TaskForm';
+import { TaskList } from 'components/TaskList/TaskList';
 
 export const App = () => {
   return (
-    <header className={css.wrapper}>
-      <section className={css.section}>
-        <h2 className={css.title}>Tasks</h2>
-        <TaskCounter />
-      </section>
-      <section className={css.section}>
-        <h2 className={css.title}>Filter by status</h2>
-        <StatusFilter />
-      </section>
-    </header>
+    <Layout>
+      <AppBar />
+      <TaskForm />
+      <TaskList />
+    </Layout>
   );
 };
